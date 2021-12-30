486473a90976f69183f143032153b030e9928787
giatkt1598 <giatkt1598@gmail.com>
2021-12-31 01:44:24 +0700

    commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <div className="App">
+      <button className="rounded-full bg-blue-500 px-4 py-2 text-white
+        font-medium shadow-xl
+        hover:bg-blue-700">Save change</button>
+    <h1 className='text-5xl font-bold underline'>
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
